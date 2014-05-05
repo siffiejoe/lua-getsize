ebee8bff56283fd1b6ac1177b3ffd6c6599a6e30
Philipp Janda <siffiejoe@gmx.net>
2014-05-05 18:17:53 +0200

    fixed output for tables with empty hash part
    include size of upvalues for Lua closures
    
    report a userdata's full memory not just its payload
    
    removed handling of Proto objects
    
    ported to Lua 5.2
    
    added simple test script

--- getsize.c
+++ getsize.c
@@ -11,52 +11,89 @@
 #include <lstate.h>
 #include <lobject.h>
 #include <lfunc.h>
+#include <lstring.h>
+
+
+/* the macro defined in lfunc.h does not count upvalues */
+#define sizeLclosureWithUpvalues(n) \
+  (cast(int, sizeof(LClosure)) - \
+   cast(int, sizeof(UpVal*)) + \
+   cast(int, (sizeof(UpVal*)+sizeof(UpVal))*(n)))
+
+
+#if LUA_VERSION_NUM == 501
+#define ARG(L, n) (((L)->base+(n))-1)
+#elif LUA_VERSION_NUM == 502
+#define ARG(L, n) ((L)->ci->func+(n))
+#endif
+
 
 int debug_getsize(lua_State* L)
 {
-  TValue* o = L->base;
-  switch (o->tt) {
-    /* Container types */
+  Node const* dummynode = lua_touserdata(L, lua_upvalueindex(1));
+  TValue* o = ARG( L, 1 );
+  switch (ttype(o)) {
     case LUA_TTABLE: {
       Table *h = hvalue(o);
       lua_pushinteger(L, sizeof(Table) + sizeof(TValue) * h->sizearray +
-                             sizeof(Node) * sizenode(h));
+                         sizeof(Node) * (h->node == dummynode ? 0 : sizenode(h)));
       break;
     }
+#if LUA_VERSION_NUM == 501
     case LUA_TFUNCTION: {
       Closure *cl = clvalue(o);
       lua_pushinteger(L, (cl->c.isC) ? sizeCclosure(cl->c.nupvalues) :
-                           sizeLclosure(cl->l.nupvalues));
+                           sizeLclosureWithUpvalues(cl->l.nupvalues));
       break;
     }
+#elif LUA_VERSION_NUM == 502
+    case LUA_TLCL: { /* Lua closure */
+      Closure *cl = clvalue(o);
+      lua_pushinteger(L, sizeLclosureWithUpvalues(cl->l.nupvalues));
+      break;
+    }
+    case LUA_TLCF: { /* light C function */
+      lua_pushinteger(L, sizeof(lua_CFunction));
+      break;
+    }
+    case LUA_TCCL: { /* C closure */
+      Closure *cl = clvalue(o);
+      lua_pushinteger(L, sizeCclosure(cl->c.nupvalues));
+      break;
+    }
+#endif
+#if LUA_VERSION_NUM == 501
     case LUA_TTHREAD: {
       lua_State *th = thvalue(o);
       lua_pushinteger(L, sizeof(lua_State) + sizeof(TValue) * th->stacksize +
                                  sizeof(CallInfo) * th->size_ci);
       break;
     }
-    case LUA_TPROTO: {
-      Proto *p = pvalue(o);
-      lua_pushinteger(L, sizeof(Proto) + sizeof(Instruction) * p->sizecode +
-                             sizeof(Proto *) * p->sizep +
-                             sizeof(TValue) * p->sizek + 
-                             sizeof(int) * p->sizelineinfo +
-                             sizeof(LocVar) * p->sizelocvars +
-                             sizeof(TString *) * p->sizeupvalues);
-     break;
+#elif LUA_VERSION_NUM == 502
+    case LUA_TTHREAD: {
+      lua_State *th = thvalue(o);
+      CallInfo *ci = th->base_ci.next;
+      size_t cisize = 0;
+      for (; ci != NULL; ci = ci->next)
+        cisize += sizeof(CallInfo);
+      lua_pushinteger(L, sizeof(lua_State) + sizeof(TValue) * th->stacksize +
+                         cisize);
+      break;
     }
-    /* Non-containers */
+#endif
     case LUA_TUSERDATA: {
-      lua_pushnumber(L, uvalue(o)->len);
+      lua_pushinteger(L, sizeudata(uvalue(o)));
       break;
     }
     case LUA_TLIGHTUSERDATA: {
-      lua_pushnumber(L, sizeof(void*));
+      lua_pushinteger(L, sizeof(void*));
       break;
     }
+#if LUA_VERSION_NUM == 502
+    case LUA_TLNGSTR: /* fall through */
+#endif
     case LUA_TSTRING: {
-      TString *s = rawtsvalue(o);
-      lua_pushinteger(L, sizeof(TString) + s->tsv.len + 1);
+      lua_pushinteger(L, sizestring(tsvalue(o)));
       break;
     }
     case LUA_TNUMBER: {
@@ -74,8 +111,16 @@ int debug_getsize(lua_State* L)
 
 int luaopen_getsize(lua_State* L)
 {
+	lua_settop(L, 0);
 	lua_getglobal(L, "debug");
-	lua_pushcfunction(L, debug_getsize);
-	lua_setfield(L, -2, "getsize");
-	return 0;
+	lua_createtable(L, 0, 0); /* to get dummynode pointer */
+	lua_pushlightuserdata(L, hvalue(ARG(L,2))->node);
+	lua_pushcclosure(L, debug_getsize, 1);
+	lua_replace(L, -2); /* remove dummy table */
+	if (lua_type(L, 1) == LUA_TTABLE) {
+		lua_pushvalue(L, -1);
+		lua_setfield(L, 1, "getsize");
+	}
+	return 1;
 }
+
---
+++ tests/test.lua
@@ -0,0 +1,37 @@
+#!/usr/bin/lua
+
+local maj,min = assert( _VERSION:match( "(%d+)%.(%d+)$" ) )
+package.cpath = "../?-"..maj..min..".so;"..package.cpath
+local size = require( "getsize" )
+local dummy
+
+local function f1()
+  coroutine.yield()
+end
+
+local function f2()
+  f1()
+end
+
+local th = coroutine.create( function() f2() end )
+coroutine.resume( th )
+
+
+print( "{}", size( {} ) )
+print( "{ 1 }", size( { 1 } ) )
+print( "{ 1, 2 }", size( { 1, 2 } ) )
+print( "{ x=1 }", size( { x=1 } ) )
+print( "{ x=1, y=2 }", size( { x=1, y=2 } ) )
+print( "{ x=1, y=2, z=3 }", size( { x=1, y=2, z=3 } ) )
+print( "io.stdout", size( io.stdout ) )
+print( "print", size( print ) )
+print( "size", size( size ) )
+print( "[0 upvalues]", size( function() return end ) )
+print( "[1 upvalue]", size( function() return size end ) )
+print( "[2 upvalues]", size( function() return size, dummy end ) )
+print( "[used thread]", size( th ) )
+th = coroutine.create( function() end )
+print( "[new thread]", size( th ) )
+print( "[30 chars]", size( ("x"):rep( 30 ) ) )
+print( "[60 chars]", size( ("y"):rep( 60 ) ) )
+
